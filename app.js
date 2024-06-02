f8bdc64d47c74566734002d4ec6be1b4d882a6b7
Anshulsharma82 <anshulsharma4445@gmail.com>
2024-06-02 10:44:05 +0530

    added some extra security

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const handleError = require('./middleware/handle-error')
 const authenticateUser = require('./middleware/authorization')
 const app = express()
 
+// Extra Security Package----------------------
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const { rateLimit } = require('express-rate-limit')
+
+app.set('trust proxy',1)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+// To limit the request as per this limit is 100 per 15 min.
+app.use(rateLimit({
+    windowMs: 15*60*1000, //15 min
+    limit:100 // limit 100 requests per 15 min
+}))
+//--------------------------------------------
 app.use(express.json())
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/jobs', authenticateUser, jobsRoute)
